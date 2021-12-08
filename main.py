f0ad10bff819dbfd9965f9512b3b3de55191aa17
Jeremy Tygh <55996049+JeremyTygh@users.noreply.github.com>
2021-12-08 17:46:46 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import init
 from google.cloud import datastore
 
 app = flask.Flask(__name__)
+app.secret_key = b'oaijrwoizsdfmdfgdfyrehfghf34foinmzsdv98j234ijefjieojEFIuhEhefiueuieuheuiheu8'
 userList = {} #a list of all users in the system. added to upon receiving a valid connection. never shrinks, resets on startup.
 
 #uncomment these lines to verify that things get added to datastore!
@@ -20,9 +21,18 @@ def root():
     # use render_template to convert the template code to HTML.
     # this function will look in the templates/ folder for your file.
     # return flask.render_template('homepage.html', page_title='Main Page')
-    if flask.session.get('user') is None:
-        return flask.render_template('signin.html')
-    return flask.render_template('homepage.html', clothes=init.clothes, index=userList[flask.session['user']].index)
+    # if flask.session.get('user') is None:
+    #     return flask.render_template('signin.html')
+    # return flask.render_template('homepage.html', clothes=init.clothes, index=userList[flask.session['user']].index)
+
+    user = flask.session.get('user', None)
+    # if flask.session.get('user') is None:
+    if user:
+        return flask.render_template('homepage.html', clothes=init.clothes, index=userList[flask.session['user']].index)
+        # return flask.render_template('signin.html')
+    return flask.render_template('signin.html')
+    # return flask.render_template('homepage.html', clothes=init.clothes, index=userList[flask.session['user']].index)
+    # return flask.redirect("homepage.html", clothes=init.clothes, index=us.index)
 
 
 @app.route('/profile_screen.html')
@@ -41,9 +51,13 @@ def like():
 #for accessing the homepage, the list of all clothes is in variable clothes and the index is where in the list the page should start showing new clothes
 @app.route('/homepage.html')
 def home():
-    if flask.session.get('user') is None:
-        return flask.render_template('signin.html')
-    return flask.render_template('homepage.html', clothes=init.clothes, index=userList[flask.session['user']].index)
+    user = flask.session.get('user', None)
+    # if flask.session.get('user') is None:
+    if user:
+        return flask.render_template('homepage.html', clothes=init.clothes, index=userList[flask.session['user']].index)
+        # return flask.render_template('signin.html')
+    return flask.render_template('signin.html')
+    # return flask.render_template('homepage.html', clothes=init.clothes, index=userList[flask.session['user']].index)
     # return flask.redirect("homepage.html", clothes=init.clothes, index=us.index)
 
 #for sending the sign in screen
@@ -63,6 +77,7 @@ def login():
     username = response["username"]
     password = response["password"]
     client = datastore.Client()
+    print(username + " " + password)
 
     query = client.query(kind = 'user')
     query.add_filter("username", "=", username)
@@ -77,7 +92,7 @@ def login():
         userList[username] = user.User(username, index) #add user to the userlist
         return flask.redirect('/')
     
-    return flask.render_template('register.html', invalid=True) #return an invalid flag when passwords do not match
+    return flask.render_template('signin.html', invalid=True) #return an invalid flag when passwords do not match
     
 #for handling register requests - send username and password through json post request
 @app.route('/register', methods=['POST'])
@@ -90,7 +105,7 @@ def register():
     query = client.query(kind = 'user')
     query.add_filter("username", "=", username) #query existing users with that username
     if query.fetch().num_results > 0: #if there is any result then the username already exists
-        return flask.render_template('register.html', invalid=True) #return an invalid flag when user already exists
+        return flask.render_template('signup.html', invalid=True) #return an invalid flag when user already exists
 
     key = client.key('user', username)
     toUpload = datastore.Entity(key)
@@ -111,7 +126,7 @@ def received_like():
         print(request.get_json())  # parse as JSON
         response = request.get_json()
         userList[flask.session['user']].add_like( init.clothes[int(response["imageAddress"])] )
-        return 'OK', 200    
+        return 'OK', 200
 
     userList[flask.session['user']].viewed_item()
 
